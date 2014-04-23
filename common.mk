dc4ce6a345988d2a967c58a4f6eeac6ca7f77ce9
Alberto97 <albertop2197@gmail.com>
2014-04-23 16:50:11 +0200

    Too changes u.u

--- common.mk
+++ common.mk
@@ -39,12 +39,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/lpm.rc:root/lpm.rc
 
-
 # Init Files
 PRODUCT_COPY_FILES := $(COMMON_PATH)/rootdir/init.local.rc:root/init.local.rc
-
-
-# $(PREBUILT_PATH)/etc/init.local.rc:root/init.local.rc
 
 # Graphics
 PRODUCT_PACKAGES += \
