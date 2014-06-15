60a1307bb9a2dd7489e440a6621e9da02cf8b56c
Robert Rozic <rooobertinhooo@gmail.com>
2014-06-15 14:04:06 +0200

    Move SELinux to common
    @Paul-L
    
    Conflicts:
    	BoardCommonConfig.mk
    	common.mk
    
    Change-Id: I9c15579bd7f3f779bed8aafa273c2f3899ee9d32

--- common.mk
+++ common.mk
@@ -188,9 +188,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.kernel.android.checkjni=0 \
     dalvik.vm.checkjni=false
 
-# Disable SELinux
+# SELinux
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.boot.selinux=disabled
+    ro.boot.selinux=permissive
     
 # Disable Mobile Data at first boot
 PRODUCT_PROPERTY_OVERRIDES += \
